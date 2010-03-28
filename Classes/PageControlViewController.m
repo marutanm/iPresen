d388d73bbd68beaa4afe01b3344f8d0a1aee6b97
marutanm <marutanm@gmail.com>
2010-03-28 12:24:45 +0900

    deleted unused code

--- Classes/PageControlViewController.m
+++ Classes/PageControlViewController.m
@@ -6,30 +6,28 @@
     [super loadView];
     
     // rotate view to fit screen
-    UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
     CGAffineTransform transform = self.view.transform;
     transform = CGAffineTransformRotate(transform, (M_PI * 90 / 180.0));
     self.view.transform = transform;
-
-    // set center of view to fit screen
-    // self.view.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2);
 
     CGRect frame = self.view.frame;
     pages = [[NSMutableArray alloc] init];
     
     for(int i = 0; i < 5; i++) {
         UIView *subview = [[UIView alloc] initWithFrame:frame];
-        subview.backgroundColor = [UIColor grayColor];
+        subview.backgroundColor = [UIColor whiteColor];
 
         // Set title for each slide
         UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, frame.size.width - 20, frame.size.height * 0.3)];
         title.text = [NSString stringWithFormat:@"%d", i];
-        title.backgroundColor = [UIColor redColor];
+        title.textColor = [UIColor whiteColor];
+        title.backgroundColor = [UIColor darkGrayColor];
 
         // set contents for each slide
         UILabel *contents = [[UILabel alloc] initWithFrame:CGRectMake(0, 400, frame.size.width * 0.9, 360)];
         contents.text = @"contents";
-        contents.backgroundColor = [UIColor blueColor];
+        contents.textColor = [UIColor redColor];
+        contents.backgroundColor = [UIColor lightGrayColor];
 
         [subview addSubview:title];
         [subview addSubview:contents];
@@ -43,9 +41,6 @@
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // return YES;
-    // return (interfaceOrientation == UIInterfaceOrientationPortrait);
-    // return (interfaceOrientation == UIInterfaceOrientationLandscapeLeft || interfaceOrientation == UIInterfaceOrientationLandscapeRight);
     return (interfaceOrientation == UIInterfaceOrientationLandscapeRight);
 }
 
--- Classes/iPresenAppDelegate.m
+++ Classes/iPresenAppDelegate.m
@@ -17,18 +17,13 @@
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {    
 
-    [[UIDevice currentDevice] setOrientation:UIInterfaceOrientationLandscapeRight];
-
     CGRect screenBounds = [[UIScreen mainScreen] bounds];
-    // NSLog([NSString stringWithFormat:@"screenBounds\norigin.x:%f\norigin.y:%f\nsize.width:%f\nsize.height%f", screenBounds.origin.x, screenBounds.origin.y, screenBounds.size.width, screenBounds.size.height]);
     self.window = [[[UIWindow alloc] initWithFrame:screenBounds] autorelease];
-    window.backgroundColor = [UIColor cyanColor];
 
     viewController = [[PageControlViewController alloc] init];
     
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
-
 }
 
 
