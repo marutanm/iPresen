efc0512c725414462e4193ead3e36b7f660212c8
marutanm <marutanm@gmail.com>
2010-03-28 23:08:12 +0900

    read data and display it

--- Classes/PageControlViewController.m
+++ Classes/PageControlViewController.m
@@ -14,12 +14,28 @@
     CGRect frame = self.view.frame;
     pages = [[NSMutableArray alloc] init];
 
-    for(int i = 0; i < 5; i++) {
-        UIView *subview = [[UIView alloc] initWithFrame:frame];
-        subview.backgroundColor = [UIColor darkGrayColor];
 
-        [subview addSubview:[self title:frame]];
-        [subview addSubview:[self contents:frame]];
+    NSArray *contents = [NSArray arrayWithObjects:@"cont1", @"cont2", nil];
+    NSArray *keys = [NSArray arrayWithObjects:@"title", @"contents", nil]; 
+    NSArray *values = [NSArray arrayWithObjects:@"this is title", contents, nil];
+    NSDictionary *page = [NSDictionary dictionaryWithObjects:values forKeys:keys];
+    NSMutableArray *slides = [NSMutableArray array];
+    [slides addObject:page];
+    [slides addObject:page];
+    [slides addObject:page];
+    [slides addObject:page];
+    // NSDictionary *dic = [slides objectAtIndex:1];
+    // NSLog(@"%@", [dic objectForKey:@"title"]);
+
+
+
+    for(int i = 0; i < slides.count; i++) {
+        UIView *subview = [[UIView alloc] initWithFrame:frame];
+        subview = [self prepareSlide:subview dict:[slides objectAtIndex:i]];
+
+        // UILabel *title = [self title:frame dict:[slides objectAtIndex:i]];
+        // UILabel *contents = [self contents:frame];
+
         [pages addObject:subview];    
     }
 
@@ -37,14 +53,24 @@
     XMLParser *doc = [[XMLParser alloc] initWithXMLString:url];
     
 
-
     scrollView = [[PageScrollView alloc] initWithFrame:self.view.frame];
     scrollView.pages = pages;
     scrollView.delegate = self;
     self.view = scrollView;
 }
 
-- (id) title:(CGRect)frame {
+- (id) prepareSlide:(UIView *)view dict:(NSDictionary *)dict{
+    view.backgroundColor = [UIColor darkGrayColor];
+    UILabel *title = [self title:view.frame text:[dict objectForKey:@"title"]];
+    // title.text = [dict objectForKey:@"title"];
+    UILabel *contents = [self contents:view.frame array:[dict objectForKey:@"contents"]];
+
+    [view addSubview:title];
+    [view addSubview:contents];
+    return view;
+}
+
+- (id) title:(CGRect)frame text:(NSString *)text{
     float margin = 10;
     // Set title for each slide
     UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(margin, margin, frame.size.width - margin * 2, frame.size.height * 0.3)];
@@ -55,23 +81,28 @@
     title.font = [UIFont boldSystemFontOfSize:frame.size.height * 0.3];
     title.adjustsFontSizeToFitWidth = YES;
     title.minimumFontSize = frame.size.height * 0.3 / 10;
-    title.text = @"hoge\nfuga\nhoge";
+    title.text = text;
     return title;
 }
 
-- (id) contents:(CGRect)frame {
+- (id) contents:(CGRect)frame array:(NSArray *)array{
     float margin = 50;
+    float height = (frame.size.height * 0.6 - margin) / [array count];
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(margin, frame.size.height * 0.4, frame.size.width - margin * 2, frame.size.height * 0.6 - margin)];
+    for (int i = 0; i < [array count]; i++) {
     // set contents for each slide
-    UILabel *contents = [[UILabel alloc] initWithFrame:CGRectMake(margin, frame.size.height * 0.4, frame.size.width - margin * 2, frame.size.height * 0.6 - margin)];
-    contents.backgroundColor = [UIColor clearColor];
-    contents.textColor = [UIColor whiteColor];
-    contents.font = [UIFont systemFontOfSize:frame.size.height * 0.1];
-    contents.adjustsFontSizeToFitWidth = YES;
-    contents.minimumFontSize = frame.size.height * 0.3 / 10;
-    contents.lineBreakMode = UILineBreakModeWordWrap;
-    contents.numberOfLines = 0;
-    contents.text = @"cont\nents";
-    return contents;
+        UILabel *contents = [[UILabel alloc] initWithFrame:CGRectMake(0, i * height, frame.size.width - margin * 2, height)];
+        contents.backgroundColor = [UIColor clearColor];
+        contents.textColor = [UIColor whiteColor];
+        contents.font = [UIFont systemFontOfSize:frame.size.height * 0.1];
+        contents.adjustsFontSizeToFitWidth = YES;
+        contents.minimumFontSize = frame.size.height * 0.3 / 10;
+        contents.lineBreakMode = UILineBreakModeWordWrap;
+        contents.numberOfLines = 0;
+        contents.text = [array objectAtIndex:i];
+        [view addSubview:contents];
+    }
+    return view;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
